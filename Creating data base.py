728e716057b96b2ce20167b3b966300171debe02
Janisar753 <109617273+Janisar753@users.noreply.github.com>
2022-12-17 09:39:10 +0530

    This is my first git push

---
+++ Creating data base.py
@@ -0,0 +1,18 @@
+create table if not     exists bank_details (
+age int ,
+job varchar(30) ,
+marital varchar(30) ,
+education varchar(30),
+`default` varchar(30),
+balance int,
+housing varchar(30) ,
+loan varchar(30) ,
+contact varchar(30) ,
+`day` int ,
+`month` varchar(30) ,
+duration int ,
+campaign int ,
+pdays int ,
+previous int ,
+poutcome varchar(30),
+y varchar(30))
